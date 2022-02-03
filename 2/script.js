38eb40793ef2449bc30e2ecc0c4abb0890585d63
= <=>
2022-02-03 11:57:11 +0200

    changing variables

--- 2/script.js
+++ 2/script.js
@@ -3,23 +3,24 @@ let number1 = Number(prompt("ваше первое число?"));
 let number2 = Number(prompt("ваше второе число?"));
 alert("выбырите действие которые вам нужно")
 let action = Number(prompt("добавление 1, вычитание 2, диление 3, умножение 4"));
+let result;
 
 switch (action) {
     case 1:
-        let sum1 = (number1 + number2);
-        alert("результат: " + sum1);
+        result = (number1 + number2);
+        alert("результат: " + result);
         break;
     case 2:
-        let sum2 = (number1 - number2);
-        alert("результат: " + sum2);
+        result = (number1 - number2);
+        alert("результат: " + result);
         break;
     case 3:
-        let sum3 = (number1 / number2);
-        alert("результат: " + sum3);
+        result = (number1 / number2);
+        alert("результат: " + result);
         break;
     case 4:
-        let sum4 = (number1 * number2);
-        alert("результат: " + sum4);
+        result = (number1 * number2);
+        alert("результат: " + result);
         break;
     default:
         alert("некоректные значения");
