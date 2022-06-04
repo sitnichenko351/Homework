d20e4382786d04f886513cb91200a2929a42eec7
sitnichenko351 <98640680+sitnichenko351@users.noreply.github.com>
2022-06-04 18:28:11 +0300

    11 HW

--- 1/script.js
+++ 1/script.js
@@ -1,135 +1,46 @@
-const actionButton = document.querySelector("button");
-actionButton.addEventListener("click", library);
-
-const books = [
-  { id: 1, author: "Фицджеральд", name: "Великий Гетсби", isReading: false },
-  { id: 2, author: "Толстой", name: "Анна Каренина", isReading: false },
-  { id: 3, author: "Оруел", name: "1984", isReading: false },
-  { id: 4, author: "Сервантес", name: "Дон Кихот", isReading: false },
+const products = [
   {
-    id: 5,
-    author: "Достоевские",
-    name: "Преступление и наказание",
-    isReading: false,
+    name: "Iphone 12",
+    brand: "Apple",
+    price: 3200000,
+    properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
+  },
+  {
+    name: "Galaxy S20 Ultra",
+    brand: "Samsung",
+    price: 2900000,
+    properties: ["120 hz screen", "Water resistance"],
+  },
+  {
+    name: "MI 9",
+    brand: "Xiaomi",
+    price: 1300000,
+    properties: ["Best price", "Pay less - get more!"],
   },
 ];
 
-function library() {
-  let action = prompt("Action?");
+const renderProcuts = (productsList) => {
+  const container = document.createElement("div");
 
-  if (action === null) {
-    alert("Bye!");
+  productsList.forEach((prod) => {
+    const prodContainer = document.createElement("div");
+    const prodTitle = document.createElement("h3");
+    const prodSubtitle = document.createElement('h4');
+    const prodPropertiesList = document.createElement("ul");
 
-    return;
-  }
+    prod.properties.forEach((prop) => {
+      const listItem = document.createElement("li");
+      listItem.innerText = prop;
+      prodPropertiesList.append(listItem);
+    });
 
-  action = action.toLowerCase().trim();
+    prodTitle.innerText = prod.name;
+    prodSubtitle.innerText = prod.brand;
 
-  switch (action) {
-    case "take":
-      takeBook();
-      break;
-    case "return":
-      returnBook();
-      break;
-    case "add":
-      addBook();
-      break;
-    default:
-      alert("No action!");
-  }
-}
-
-const takeBook = () => {
-  const availableBooksNames = books
-    .filter((book) => !book.isReading)
-    .map((book) => ` - ${book.name}`)
-    .join("\n");
-
-  let desiredBookName = prompt(`Enter book name:\n${availableBooksNames}`);
-
-  if (!desiredBookName) {
-    alert("No book name");
-
-    return;
-  }
-
-  desiredBookName = desiredBookName.trim().toLowerCase();
-
-  const desiredBook = books.find((book) => {
-    return book.name.toLowerCase() === desiredBookName;
+    prodContainer.append(prodTitle, prodSubtitle, prodPropertiesList);
+    container.append(prodContainer)
   });
 
-  if (!desiredBook) {
-    alert("No book with this name");
-
-    return;
-  }
-
-  if (desiredBook.isReading) {
-    alert("Sorry, this book is reading by someone else");
-
-    return;
-  }
-
-  desiredBook.isReading = true;
-
-  alert(`Thanks! Your book id is ${desiredBook.id}`);
-
-  console.log(`books`, books);
+  document.body.append(container);
 };
-
-const returnBook = () => {
-  const returningBookId = Number(prompt("Enter book id:"));
-
-  if (!returningBookId) {
-    alert("No book id");
-
-    return;
-  }
-
-  const currentBook = books.find((book) => book.id === returningBookId);
-
-  if (!currentBook) {
-    alert("Invalid book id");
-
-    return;
-  }
-
-  if (!currentBook.isReading) {
-    alert("This book is not reading now");
-
-    return;
-  }
-
-  currentBook.isReading = false;
-  alert(`Thanks, come again! Do you like "${currentBook.name}"? `);
-};
-const addBook = () => {
-  const name = prompt("Book name");
-  const author = prompt("Book Author");
-
-  const newBook = {
-    name,
-    author,
-    isReaading: false,
-    id: generateBookId(),
-  };
-
-  console.log(`newBook`, newBook);
-  books.push(newBook);
-
-  alert(`Book was added! Libary id : ${newBook.id}`);
-};
-
-function generateBookId () {
-  let isBookWithIdExists = true;
-  let generatedId;
-
-  do {
-    generatedId = Math.ceil(Math.random() * 1000);
-    isBookWithIdExists = Boolean(books.find((book) => book.id === generatedId));
-  } while (isBookWithIdExists);
-
-  return generatedId;
-}
+renderProcuts (products);
