d73013c83f44347aaf9638fa615323f0f3bcf24e
sitnichenko351 <98640680+sitnichenko351@users.noreply.github.com>
2022-06-06 11:02:45 +0300

    12 Домашка

--- 1/script.js
+++ 1/script.js
@@ -1,46 +1,17 @@
-const products = [
-  {
-    name: "Iphone 12",
-    brand: "Apple",
-    price: 3200000,
-    properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
-  },
-  {
-    name: "Galaxy S20 Ultra",
-    brand: "Samsung",
-    price: 2900000,
-    properties: ["120 hz screen", "Water resistance"],
-  },
-  {
-    name: "MI 9",
-    brand: "Xiaomi",
-    price: 1300000,
-    properties: ["Best price", "Pay less - get more!"],
-  },
-];
+const createTooltip = (element, text) => {
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltip");
+  tooltip.innerText = text;
+  document.body.append(tooltip);
+  const anchorElemCoords = element.getBoundingClientRect();
+  const tooltipTop = anchorElemCoords.top - 10 - tooltip.clientHeight;
+  const tooltipLeft = 
+    anchorElemCoords.left +
+    anchorElemCoords.width / 2 -
+    tooltip.clientWidth / 2;
 
-const renderProcuts = (productsList) => {
-  const container = document.createElement("div");
-
-  productsList.forEach((prod) => {
-    const prodContainer = document.createElement("div");
-    const prodTitle = document.createElement("h3");
-    const prodSubtitle = document.createElement('h4');
-    const prodPropertiesList = document.createElement("ul");
-
-    prod.properties.forEach((prop) => {
-      const listItem = document.createElement("li");
-      listItem.innerText = prop;
-      prodPropertiesList.append(listItem);
-    });
-
-    prodTitle.innerText = prod.name;
-    prodSubtitle.innerText = prod.brand;
-
-    prodContainer.append(prodTitle, prodSubtitle, prodPropertiesList);
-    container.append(prodContainer)
-  });
-
-  document.body.append(container);
+    tooltip.style.top = `${tooltipTop}px`;
+    tooltip.style.left = `${tooltipLeft}px`;
 };
-renderProcuts (products);
+const button = document.getElementById("example");
+createTooltip(button, "Просто кнопка");
