9ee23fd961a0b50bc285fa3aa5b0b67670c89027
sitnichenko351 <98640680+sitnichenko351@users.noreply.github.com>
2022-06-23 13:16:39 +0300

    Homework 14

--- 1/script.js
+++ 1/script.js
@@ -1,17 +1,100 @@
-const createTooltip = (element, text) => {
-  const tooltip = document.createElement("div");
-  tooltip.classList.add("tooltip");
-  tooltip.innerText = text;
-  document.body.append(tooltip);
-  const anchorElemCoords = element.getBoundingClientRect();
-  const tooltipTop = anchorElemCoords.top - 10 - tooltip.clientHeight;
-  const tooltipLeft = 
-    anchorElemCoords.left +
-    anchorElemCoords.width / 2 -
-    tooltip.clientWidth / 2;
+const formConfig = [
+  {
+    element: "text",
+    name: "login",
+    label: "Логин",
+  },
+  {
+    element: "text",
+    name: "age",
+    label: "Возраст",
+  },
+  {
+    element: "select",
+    name: "language",
+    label: "Выберите язык програмирования",
+    options: [
+      {
+        text: "JavaScript",
+        value: "js",
+      },
+      {
+        text: "Java",
+        value: "java",
+      },
+      {
+        text: "Python",
+        value: "python",
+      },
+    ],
+  },
+];
 
-    tooltip.style.top = `${tooltipTop}px`;
-    tooltip.style.left = `${tooltipLeft}px`;
+const createFields = (config) => {
+  return config.map((fieldConfig) => {
+    const wrapper = document.createElement("div");
+    const label = document.createElement("label");
+    label.setAttribute("for", fieldConfig.name);
+    label.innerText = fieldConfig.label;
+
+    let element;
+
+    switch (fieldConfig.element) { 
+      case "text":
+        element = document.createElement("input");
+        element.setAttribute("type","text");
+
+        element.setAttribute("id", fieldConfig.name);
+        element.setAttribute("name", fieldConfig.name);
+
+        break;
+        case "select":
+          element = document.createElement("select");
+          fieldConfig.options.forEach((opt) => {
+            const option = document.createElement("option");
+            option.innerText = opt.text;
+            option.setAttribute("value", opt.value);
+
+            element.append(option);
+          });
+
+          element.setAttribute("id", fieldConfig.name);
+          element.setAttribute("name", fieldConfig.name);
+
+          break;
+        default:
+          console.log("Unhandled input type", fieldConfig.element);  
+    }
+    wrapper.append(label, element);
+    return wrapper;
+  });
 };
-const button = document.getElementById("example");
-createTooltip(button, "Просто кнопка");
+const convertFromDateToObject = (formData) => {
+  const data = {};
+  for (const pair of formData.entries()) {
+    data[pair[0]] = pair[1];
+  }
+  return data;
+};
+
+const handleSubmit = (evt) => {
+  evt.preventDefault();
+
+  const formData = new FormData(evt.target);
+  const data = convertFromDateToObject (formData);
+
+  console.log(data);
+};
+const form = document.createElement("form");
+const fields = createFields(formConfig);
+
+fields.forEach((item) => {
+  form.append(item);
+});
+
+const submitBtn = document.createElement("button");
+submitBtn.setAttribute("type", "submit");
+submitBtn.innerText = "Подтвердить";
+form.append(submitBtn);
+document.body.append(form);
+form.addEventListener("submit", handleSubmit);
