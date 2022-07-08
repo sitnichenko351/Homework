90f29eab1552ffded7fc2cb744808172031d31d7
sitnichenko351 <98640680+sitnichenko351@users.noreply.github.com>
2022-07-08 14:59:47 +0300

    15 homework

--- 1/script.js
+++ 1/script.js
@@ -1,100 +1,103 @@
-const formConfig = [
-  {
-    element: "text",
-    name: "login",
-    label: "Логин",
+// 1
+
+const response = {
+  data: [
+    {
+      username: "samuel",
+      is_active: true,
+      created_at: "2020-11-20T09:53:50.000000Z",
+    },
+    {
+      username: "john",
+      is_active: false,
+      created_at: "2020-11-20T09:53:50.000000Z",
+    },
+    {
+      username: "peter",
+      is_active: false,
+      created_at: "2020-11-20T09:53:50.000000Z",
+    },
+  ],
+  meta: {
+    paging: {
+      current: 1,
+      next: 2,
+      prev: null,
+      total: 14,
+      per_page: 3,
+    },
   },
-  {
-    element: "text",
-    name: "age",
-    label: "Возраст",
-  },
-  {
-    element: "select",
-    name: "language",
-    label: "Выберите язык програмирования",
-    options: [
-      {
-        text: "JavaScript",
-        value: "js",
-      },
-      {
-        text: "Java",
-        value: "java",
-      },
-      {
-        text: "Python",
-        value: "python",
-      },
-    ],
-  },
-];
-
-const createFields = (config) => {
-  return config.map((fieldConfig) => {
-    const wrapper = document.createElement("div");
-    const label = document.createElement("label");
-    label.setAttribute("for", fieldConfig.name);
-    label.innerText = fieldConfig.label;
-
-    let element;
-
-    switch (fieldConfig.element) { 
-      case "text":
-        element = document.createElement("input");
-        element.setAttribute("type","text");
-
-        element.setAttribute("id", fieldConfig.name);
-        element.setAttribute("name", fieldConfig.name);
-
-        break;
-        case "select":
-          element = document.createElement("select");
-          fieldConfig.options.forEach((opt) => {
-            const option = document.createElement("option");
-            option.innerText = opt.text;
-            option.setAttribute("value", opt.value);
-
-            element.append(option);
-          });
-
-          element.setAttribute("id", fieldConfig.name);
-          element.setAttribute("name", fieldConfig.name);
-
-          break;
-        default:
-          console.log("Unhandled input type", fieldConfig.element);  
-    }
-    wrapper.append(label, element);
-    return wrapper;
-  });
 };
-const convertFromDateToObject = (formData) => {
-  const data = {};
-  for (const pair of formData.entries()) {
-    data[pair[0]] = pair[1];
+
+const {
+  data: [{ is_active: isActive }],
+  meta: {
+    paging: { total }
   }
-  return data;
+} = response;
+
+// console.log(total);
+// console.log(response.meta.paging.total);
+
+// 2
+
+const user = {
+  name: "gabriel",
+  surname: "brown",
+  age: 17,
+  height: 178,
 };
 
-const handleSubmit = (evt) => {
-  evt.preventDefault();
+const { name, surname, ...parameters } = user;
 
-  const formData = new FormData(evt.target);
-  const data = convertFromDateToObject (formData);
+console.log( parameters );
 
-  console.log(data);
+// 3
+
+const max = (a, b) => {
+  return a > b ? a : b;
 };
-const form = document.createElement("form");
-const fields = createFields(formConfig);
+const advancedMax = (...numbers) => {
+  let max = 0;
 
-fields.forEach((item) => {
-  form.append(item);
+  numbers.forEach((num) => {
+    if (num > max) {
+      max = max;
+    }
+  });
+ 
+  return max;
+};
+
+console.log(advancedMax(8, 10, 22));
+
+// 4
+
+// const createMessage = (author, text, reciever, time) => {
+//   return `From ${author} to ${reciever}: ${text} (${time.toLocaleDateString()})`;
+// };
+// const message = createMessage("Peter", "Hello", "Sam", new Date());
+
+const createMessage = ({
+  author = "Guest",
+  text,
+  reciever,
+  time = new Date()
+}) => {
+  return `From ${author} to ${reciever}: ${text} (${time.toLocaleDateString()})`;
+};
+
+const message = createMessage({
+  reciever: "John",
+  text: "Hi!",
 });
 
-const submitBtn = document.createElement("button");
-submitBtn.setAttribute("type", "submit");
-submitBtn.innerText = "Подтвердить";
-form.append(submitBtn);
-document.body.append(form);
-form.addEventListener("submit", handleSubmit);
+console.log(`message`, message);
+// 5
+let regexp = /\w[\d]\w/gi
+
+let str = "x1y 722a 333 a123v1n a55555a qwe1 1zxc";
+
+let match = str.match(regexp);
+
+console.log(match)
